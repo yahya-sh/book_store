988d90b5f5cc52358d413a0b243ba7155e386ec2
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-04 15:43:25 +0300

    Init project

---
+++ book_outlet/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    rating = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ]
+    )
+    is_best_seller = models.BooleanField(default=False)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from book-store!")
+
+
+if __name__ == "__main__":
+    main()
