49686fd2c009f3eb269a8b53a7b61911798c7277
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-04 15:59:29 +0300

    implemented books index and detail views

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
+
 
 # Create your views here.
+def index(request):
+    book_list = models.Book.objects.all()
+    return render(
+        request,
+        "book_outlet/index.html",
+        {
+            "book_list": book_list,
+        },
+    )
+
+
+def detail(request, id):
+    book = get_object_or_404(models.Book, pk=id)
+    return render(request, "book_outlet/detail.html", {"book": book})
