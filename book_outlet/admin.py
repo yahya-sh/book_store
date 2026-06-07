d262c913e1503f77b05109f945fa2efba9210c6a
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-07 10:58:12 +0300

    implemented custom BookAdmin class with custom filters

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,4 +1,51 @@
 from django.contrib import admin
 from . import models
+from django.utils.translation import gettext as _
 # Register your models here.
-admin.site.register(models.Book)
+
+
+class RatingFilter(admin.SimpleListFilter):
+    parameter_name = "rating_level"
+    title = _("Rating Ranges")
+
+    def lookups(self, request, model_admin):
+        return [
+            ("bad", _("Bad Rating (1 < 3)")),
+            ("medium", _("Medium Rating (3 < 4)")),
+            ("good", _("Good Rating (4 < 5)")),
+            ("perfect", _("Perfect Rating (5)")),
+        ]
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "bad":
+            return queryset.filter(
+                rating__lt=3,
+            )
+        if value == "medium":
+            return queryset.filter(
+                rating__gte=3,
+                rating__lt=4,
+            )
+        if value == "good":
+            return queryset.filter(
+                rating__gte=4,
+                rating__lt=5,
+            )
+        if value == "perfect":
+            return queryset.filter(
+                rating__gte=5,
+            )
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "author", "rating", "is_best_seller")
+    readonly_fields = ("slug",)
+    list_filter = (
+        "is_best_seller",
+        RatingFilter,
+    )
+
+
+# admin.site.register(models.Book, BookAdmin)
