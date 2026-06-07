b2f9da10f3aefbef7dc43737ceefb075ec8a7cb0
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-07 09:41:04 +0300

    add total count, rating avg, order by name to Books List in index.html page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
+from django.db.models import Avg
 
 
 # Create your views here.
 def index(request):
-    book_list = models.Book.objects.all()
+    book_list = models.Book.objects.all().order_by('title')
+    total_count = book_list.count()
+    rating_avg = book_list.aggregate(Avg("rating")).get('rating__avg')
     return render(
         request,
         "book_outlet/index.html",
         {
             "book_list": book_list,
+            "total_count": total_count,
+            "rating_avg": rating_avg,
         },
     )
 
